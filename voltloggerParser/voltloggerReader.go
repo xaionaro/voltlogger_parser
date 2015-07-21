cf864bbabe307ad710cf8e1fb3e49de22ddb0e19
Dmitry Yu Okunev <dyokunev@mephi.ru>
2015-07-21 23:14:18 +0300

    Initial commit, pt.1

--- voltloggerParser/voltloggerReader.go
+++ voltloggerParser/voltloggerReader.go
@@ -2,11 +2,16 @@ package voltloggerReader
 
 import (
 	"os"
+	"io"
 	"fmt"
 	"encoding/binary"
 )
+/*
+const (
+	TIMESTAMP_WINDOW	int = 4096
+)*/
 
-type VoltloggerDumpRaw struct {
+type VoltloggerDumpRawHeader struct {
 	Version		byte
 	Magic		[11]byte
 	Modificators	byte
@@ -18,7 +23,12 @@ type VoltloggerDumpRaw struct {
 
 type VoltloggerDump struct {
 	DeviceName	string
-	Data		map[int]map[int]int
+	Data		map[int64][]int
+}
+
+func get16(dumpFile *os.File) (r uint16, err error) {
+	err = binary.Read(dumpFile, binary.LittleEndian, &r)
+	return r, err
 }
 
 func ParseVoltloggerDump(dumpPath string) (r VoltloggerDump, err error) {
@@ -29,8 +39,8 @@ func ParseVoltloggerDump(dumpPath string) (r VoltloggerDump, err error) {
 	}
 	defer dumpFile.Close()
 
-	// Reading binary data to a VoltloggerDumpRaw
-	var raw VoltloggerDumpRaw
+	// Reading binary header to a VoltloggerDumpRawHeader
+	var raw VoltloggerDumpRawHeader
 	err = binary.Read(dumpFile, binary.LittleEndian, &raw)
 	if (err != nil) {
 		return r, fmt.Errorf("Cannot read dump: %v", err.Error())
@@ -45,11 +55,63 @@ func ParseVoltloggerDump(dumpPath string) (r VoltloggerDump, err error) {
 	if (raw.Version != 0) {
 		return r, fmt.Errorf("Unsupported dump version: %v", raw.Version)
 	}
+	if (raw.Modificators != 0) {
+		return r, fmt.Errorf("Unsupported modificators bitmask: %o", raw.Modificators)
+	}
+	if (raw.ChannelsNum == 0) {
+		return r, fmt.Errorf("Channels number is zero")
+	}
 
+	// Parsing the Data
 
+	channelsNum := int(raw.ChannelsNum)
+	r.Data       = make(map[int64][]int)
+	var timestampGlobal int64
+	timestampGlobal = -1
+
+	for err = nil; err == nil; {
+		timestampLocal, err := get16(dumpFile)
+		if (err != nil) {
+			break
+		}
+
+		if (timestampGlobal < 0) {
+			timestampGlobal = int64(timestampLocal)
+		} else {
+			var timestampLocalDiff int
+			timestampLocalOld := int16(timestampGlobal)
+			timestampLocalDiff = int(timestampLocal) - int(timestampLocalOld)
+			/*if (timestampLocalDiff*timestampLocalDiff > TIMESTAMP_WINDOW*TIMESTAMP_WINDOW) {
+				break
+			}*/
+
+			if (timestampLocalDiff <= 0) {
+				timestampLocalDiff += (1 << 16)
+			}
+
+			timestampGlobal += int64(timestampLocalDiff);
+
+		}
+
+		values := make([]int, raw.ChannelsNum)
+		for i:=0; i < channelsNum; i++ {
+			value, err := get16(dumpFile)
+			if (err != nil) {
+				break
+			}
+			values[i] = int(value)
+		}
+
+		r.Data[timestampGlobal] = values;
+
+	}
+
+	if (err == io.EOF) {
+		err = nil
+	}
 
 	// Filling the VoltloggerDump struct
 	r.DeviceName = string(raw.DeviceName[:])
 
-	return r, nil
+	return r, err
 }
